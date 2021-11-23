9d47472e22d3a73d7e5ab98cfbb51ba4d04c49cf
Oleksii <alavir.web.dev@gmail.com>
2021-11-23 18:18:04 +0200

    delete calories chart label

--- Week_5/fitnesSiteActiveLib/js/javascript.js
+++ Week_5/fitnesSiteActiveLib/js/javascript.js
@@ -4,26 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#chart-1'),
     {
       type: 'line',
-      options: {
-        plugins: {
-          legend: {
-            display: false,
-          },
-          scales: {
-            xAxes: {
-              beginAtZero: true
-            },
-            yAxes: [{
-              beginAtZero: true,
-              ticks: {
-                min: 0,
-                max: 80,
-                stepSize: 20,
-              }
-            }]
-          }
-        }
-      },
       data: {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug'],
         datasets: [
@@ -35,55 +15,98 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         ]
       },
-    }
-  );
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  new Chart(
-    document.querySelector('.chart-2'),
-    {
-      type: 'bar',
-      data: {
-        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],
-        datasets: [
-          {
-            label: 'Calories',
-            data: [50, 70, 30, 13, 62, 30, 3, 43, 24, 45, 24, 45, 70, 30, 13, 62, 30, 3, 42, 30],
-            backgroundColor: '#0B2A97',
-            borderRadius: 20,
-            order: 1,
-            categoryPercentage: 1.0,
-            barPercentage: 0.2
-          },
-          {
-            label: 'Calories burn',
-            data: [-3, -18, -38, -8, -18, -42, -30, -16, -30, -16, -25, -3, -18, -38, -8, -18, -42, -30, -16, -42],
-            backgroundColor: '#FF9432',
-            borderRadius: 20,
-            categoryPercentage: 1.0,
-            barPercentage: 0.2
-          }]
-      },
       options: {
-        responsive: true,
         plugins: {
           legend: {
             display: false,
           },
-          scales: {
-            y: {
-              stacked: true,
-              beginAtZero: true,
-            },
-            x: {
-              stacked: true,
+        },
+        scales: {
+          xAxes: {
+            beginAtZero: true
+          },
+          yAxes: [{
+            beginAtZero: true,
+            ticks: {
+              min: 0,
+              max: 80,
+              stepSize: 20,
             }
-
-          }
+          }]
         }
-      }
-  });
+      },
+    }
+  );
 
+  new Chart(document.querySelector('.chart-2'), {
+    type: 'bar',
+    data: {
+      labels: [
+        '01',
+        '02',
+        '03',
+        '04',
+        '05',
+        '06',
+        '07',
+        '08',
+        '09',
+        '10',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '16',
+        '17',
+        '18',
+        '19',
+        '20',
+      ],
+      datasets: [
+        {
+          barPercentage: 0.2,
+          label: 'Calories',
+          data: [
+            50, 70, 30, 13, 62, 30, 3, 43, 24, 45, 24, 45, 70, 30, 13, 62, 30,
+            3, 42, 30,
+          ],
+          backgroundColor: '#0B2A97',
+          borderRadius: 20,
+        },
+        {
+          barPercentage: 0.2,
+          label: 'Calories burn',
+          data: [
+            -3, -18, -38, -8, -18, -42, -30, -16, -30, -16, -25, -3, -18, -38,
+            -8, -18, -42, -30, -16, -42,
+          ],
+          backgroundColor: '#FF9432',
+          borderRadius: 20,
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      responsive: true,
+      scales: {
+        y: {
+          stacked: true,
+          beginAtZero: true,
+        },
+        x: {
+          stacked: true,
+        },
+      },
+    },
+  })
 });
+
+
+
+
+
